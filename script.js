0f2608e801436a4e6d5c07eaba7bdb05cf1cbc7d
MeerKatten <123115613+MeerKatten@users.noreply.github.com>
2023-05-18 12:35:06 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,40 @@
+function generateQuestion() {
+  const num1 = Math.floor(Math.random() * 15) + 10;
+  const num2 = Math.floor(Math.random() * 15) + 10;
+  const answer = num1 * num2;
+
+  return {
+    question: `${num1} x ${num2}`,
+    answer,
+  };
+}
+
+let currentQuestion = generateQuestion();
+let timer;
+
+function startTimer() {
+  timer = setTimeout(() => {
+    currentQuestion = generateQuestion();
+    startTimer();
+  }, 20000);
+}
+
+function checkAnswer() {
+  const userAnswer = parseInt(document.getElementById('answer').value);
+
+  if (userAnswer === currentQuestion.answer) {
+    document.getElementById('question').textContent = currentQuestion.question;
+    document.getElementById('answer').value = '';
+    console.log('Here is the next clue');
+    clearTimeout(timer);
+    startTimer();
+    currentQuestion = generateQuestion();
+  } else {
+    console.log('Incorrect answer');
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  document.getElementById('question').textContent = currentQuestion.question;
+  startTimer();
+});
